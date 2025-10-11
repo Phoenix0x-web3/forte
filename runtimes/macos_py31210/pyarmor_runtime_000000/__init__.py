d8cf804ab1fc4dfabb6583bfe7f616a0c485000a
phoenix0x1 <221748789+phoenix0x1@users.noreply.github.com>
2025-10-11 14:02:06 +0000

    Automated update

--- runtimes/macos_py31210/pyarmor_runtime_000000/__init__.py
+++ runtimes/macos_py31210/pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.1.9 (trial), 000000, 2025-10-08T15:29:04.620934
+# Pyarmor 9.1.9 (trial), 000000, 2025-10-11T14:01:32.989045
 from .pyarmor_runtime import __pyarmor__
--- runtimes/windows_py31210/pyarmor_runtime_000000/__init__.py
+++ runtimes/windows_py31210/pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.1.9 (trial), 000000, 2025-10-08T15:29:47.347353
+# Pyarmor 9.1.9 (trial), 000000, 2025-10-11T14:01:50.775006
 from .pyarmor_runtime import __pyarmor__
